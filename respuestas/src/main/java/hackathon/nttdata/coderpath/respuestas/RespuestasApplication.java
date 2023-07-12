4110813cf80f17c79db94e03e2be9454fc4c1db5
joffre <joffre.hermosilla@gmail.com>
2023-07-12 03:57:42 -0500

    chore[0] primera configuracion

---
+++ respuestas/src/main/java/hackathon/nttdata/coderpath/respuestas/RespuestasApplication.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.respuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespuestasApplication.class, args);
+	}
+
+}
---
+++ respuestas/src/test/java/hackathon/nttdata/coderpath/respuestas/RespuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package hackathon.nttdata.coderpath.respuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
